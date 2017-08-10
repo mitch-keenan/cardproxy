4e7f53428f76aa9088edcb7ee36262ede1f1a3b8
Mitch Keenan <konamacona@gmail.com>
2017-08-10 02:13:07 -0300

    dist -> docs for github pages

--- gulpfile.js
+++ gulpfile.js
@@ -8,5 +8,5 @@ gulp.task('watch', ['webpack'], function() {
 gulp.task('webpack', function() {
   return gulp.src('src/index.js')
 		.pipe(webpack( require('./webpack.config.js') ))
-		.pipe(gulp.dest('dist/'));
+		.pipe(gulp.dest('docs/'));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -26,25 +26,6 @@ module.exports = {
         test: /\.svg$/,
         loader: 'svg-url-loader'
       }
-      /*
-      ,
-      {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      }
-      */
     ]
   },
 };
